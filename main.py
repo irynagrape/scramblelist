c1d739168521280efe5a931533f729f8ac5e5376
irynagrape <109566237+irynagrape@users.noreply.github.com>
2022-10-24 20:44:39 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+# Swap function
+
+def swapList(newList):
+    size = len(newList)
+
+    # Swapping
+
+    temp = newList[0]
+
+    newList[0] = newList[size - 1]
+
+    newList[size - 1] = temp
+
+    return newList
+
+
+# Driver code
+
+newList = []
+
+newList = [int(item) for item in input("Enter the list items : ").split()]
+
+print(swapList(newList))
